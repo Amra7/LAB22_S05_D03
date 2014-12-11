064c47450b74008ee533072cc6cffe38f906e98a
Amra7 <amrapop@gmail.com>
2014-12-11 09:55:59 +0100

    Second

--- src/Vjezbe/Datum.java
+++ src/Vjezbe/Datum.java
@@ -1,34 +1,94 @@
 package Vjezbe;
 
-import Benjo.Student;
-
 public class Datum {
 
 	private int dan;
 	private int mjesec;
 	private int godina =2014;
+	
+	public Datum(){
+		
+	}
 
-	public Datum (){
-		this.mjesec= (int)(1 +Math.random() *12);
-		
-		if ( this.mjesec ==1 || this.mjesec ==3 ||this.mjesec ==5 ||this.mjesec ==7 || this.mjesec ==8 || this.mjesec ==10 || this.mjesec ==12){
-			this.dan =(int ) (1 +Math.random() * 31);		
-		}
-		
-		if ( this.mjesec ==2 ){
-			this.dan =(int ) (1 +Math.random() * 28);
-		}
-		if ( this.mjesec ==4 || this.mjesec ==6 ||this.mjesec ==9 ||this.mjesec ==11){
-			this.dan =(int ) (1 + Math.random() * 30);
-		}
+	public Datum RandomDatum (){
+		int brojac =0;
+		do{
+			this.mjesec= (int)(1 +Math.random() *12);
+			if ( this.mjesec ==1 || this.mjesec ==3 ||this.mjesec ==5 ||this.mjesec ==7 || this.mjesec ==8 || this.mjesec ==10 || this.mjesec ==12){
+				this.dan =(int ) (1 +Math.random() * 31);		
+			}
+			
+			if ( this.mjesec ==2 ){
+				this.dan =(int ) (1 +Math.random() * 28);
+			}
+			if ( this.mjesec ==4 || this.mjesec ==6 ||this.mjesec ==9 ||this.mjesec ==11){
+				this.dan =(int ) (1 + Math.random() * 30);
+				
+				brojac ++;
+			}
+			
+		} while (brojac ==50);
+		return this;
 	}
 	
-	public  void bubbleSort(int dan, int mjesec, int godina) {
+	public int IntDatum(){
+		return this.brojDanaZaMjesece()+this.dan;
+	}
+	
+	private int brojDanaZaMjesece(){
+		int suma=0;
+		for (int i=1; i<this.mjesec;i++){
+			suma+=Datum.brojDanaZaJedanMjesec(i);
+		}
+		return suma;
+	}
+	
+	private static int brojDanaZaJedanMjesec(int trenutniMjesec){
+		if(trenutniMjesec==1)
+			return 31;
+		if (trenutniMjesec==2)
+			return 28;
+		if (trenutniMjesec==3)
+			return 31;
+		if (trenutniMjesec==4)
+			return 30;
+		if (trenutniMjesec==5)
+			return 31;
 		
+		if (trenutniMjesec==6)
+			return 30;
+		if (trenutniMjesec==7)
+			return 31;
+		if (trenutniMjesec==8)
+			return 31;
+		
+		if (trenutniMjesec==9)
+			return 30;
+		if (trenutniMjesec==10)
+			return 31;
+		if (trenutniMjesec==11)
+			return 30;
+		if (trenutniMjesec==12)
+			return 31;
+		
+		return 0;
+	}
+	
+	public static void bubbleSortStudenti(Datum[] datumi) {
+		for (int i = 0; i < datumi.length; i++) {
+			for (int j = i + 1; j < datumi.length; j++) {
+				if (datumi[i].IntDatum() > datumi[j].IntDatum()) {
+					Datum temp = datumi[i];
+					datumi[i] = datumi[j];
+					datumi[j] = temp;
+				}
+			}
+
+		}
 
 	}
 	
-//	public Datum() {
+//	public Datum() {s
 //		dan = 1;
 //		mjesec = 1;
 //		godina = 1900;
--- src/Vjezbe/TestDatum.java
+++ src/Vjezbe/TestDatum.java
@@ -1,34 +1,55 @@
 package Vjezbe;
 
 public class TestDatum {
-public static void main(String[] args) {
-	
-//	Datum prviDatum = new Datum (10, 12, 2014);
-	
-//	Datum drugiDatum = new Datum(10, 12 ,2014);
-	
-//	System.out.println(prviDatum.toString());
-//	
-//	System.out.println(prviDatum.getDan());
-//	int a = prviDatum.getDan();
-//	a = a+10;
-//	System.out.println("a:" + a);
-//	
-//	prviDatum.setDan(18);
-//	System.out.println(prviDatum.getDan());
-	
-//	prviDatum.setMjesec(3);
-//	System.out.println(prviDatum.getMjesec());
-	
-//	if(prviDatum.equals(drugiDatum)== true){
-//		System.out.println("Jednaki!");
-//	}else{
-//		System.out.println("Nejednaki!");
-//	}
-//	
-//	System.out.println(prviDatum.compare(drugiDatum));
-//	
-	Datum datum1 = new Datum ();
-	System.out.println(datum1.toString());
-}
+	public static void main(String[] args) {
+
+		// Datum prviDatum = new Datum (10, 12, 2014);
+
+		// Datum drugiDatum = new Datum(10, 12 ,2014);
+
+		// System.out.println(prviDatum.toString());
+		//
+		// System.out.println(prviDatum.getDan());
+		// int a = prviDatum.getDan();
+		// a = a+10;
+		// System.out.println("a:" + a);
+		//
+		// prviDatum.setDan(18);
+		// System.out.println(prviDatum.getDan());
+
+		// prviDatum.setMjesec(3);
+		// System.out.println(prviDatum.getMjesec());
+
+		// if(prviDatum.equals(drugiDatum)== true){
+		// System.out.println("Jednaki!");
+		// }else{
+		// System.out.println("Nejednaki!");
+		// }
+		//
+		// System.out.println(prviDatum.compare(drugiDatum));
+		//
+		Datum datum1 = new Datum();
+		System.out.println(datum1.toString());
+
+		Datum[] dani = napraviNiz(50);
+		Datum.bubbleSortStudenti(dani);
+		
+		for (int i = 0; i < dani.length; i++) {
+            if ( i!=0){
+           	 System.out.println("Razlika: " + (dani[i].IntDatum() - dani[i-1].IntDatum()) );
+            }
+             System.out.println("Dan je: " + dani[i]);
+
+            
+		}
+	}
+
+	private static Datum[] napraviNiz(int brojDatuma) {
+		Datum[] nizDana = new Datum[50];
+		for (int i = 0; i < nizDana.length; i++) {
+			nizDana[i] = new Datum().RandomDatum();
+
+		}
+		return nizDana;
+	}
 }
